209dd62fe64e488e74541298c4febf7bd79d8385
JiatongSun <SJT_sjtu1998@163.com>
2020-04-01 00:38:24 -0400

    fix bug

--- HW5/HW5_code/hw5_solutions.py
+++ HW5/HW5_code/hw5_solutions.py
@@ -88,3 +88,35 @@ class CNNClassifier(torch.nn.Module):
         out = out.view(out.size(0),-1)
         out = self.fc(out)
         return out
+
+def test_logging(train_logger, valid_logger):
+    global_step = 0
+    size_train = 20
+    size_valid = 10
+    for epoch in range(10):
+        torch.manual_seed(epoch)
+        train_acc = []
+        valid_acc = []
+        for iteration in range(size_train):
+            dummy_train_loss = 0.9**(epoch+iteration/20.)
+            dummy_train_accuracy = epoch/10. + torch.randn(10)
+            train_logger.add_scalar('train loss',
+                                    dummy_train_loss,
+                                    global_step)
+            train_acc.append(torch.mean(dummy_train_accuracy))
+            global_step += 1
+        
+        train_acc_iter = torch.mean(torch.FloatTensor(train_acc))
+        train_logger.add_scalar('train accuracy',
+                                train_acc_iter,
+                                epoch)
+        
+        torch.manual_seed(epoch)
+        for iteration in range(size_valid):
+            dummy_validation_accuracy = epoch / 10. + torch.randn(10)
+            valid_acc.append(torch.mean(dummy_validation_accuracy))
+            
+        valid_acc_iter = torch.mean(torch.FloatTensor(valid_acc))
+        valid_logger.add_scalar('valid accuracy',
+                                valid_acc_iter,
+                                epoch)
